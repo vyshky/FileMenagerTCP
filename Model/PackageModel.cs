9c2b4ff2735fdd52d81c6503eadc7bdd82f1e666
Alexander <Aleksandr.Dedyukhin@vsegda-da.com>
2022-07-03 15:07:04 +0400

    изменил Post и Get методы - сократил код

--- Model/PackageModel.cs
+++ Model/PackageModel.cs
@@ -2,26 +2,25 @@
 {
     public class PackageModel
     {
-        private Dictionary<string, Action> dictionary = new Dictionary<string, Action>();
-        private Package package = new Package();
-      
+        private static Dictionary<string, Action<Package>> dictionary = new Dictionary<string, Action<Package>>();       
         public PackageModel()
         {
-            dictionary.Add("get", Get);
-            dictionary.Add("post", Post);
+            dictionary.Add("get", new Action<Package>(package => Get(package)));
+            dictionary.Add("post", new Action<Package>(package => Post(package)));            
         }
-        public void Execute(Package _package)
+        public static void Execute(Package _package)
         {
-            package = _package ?? throw new ArgumentNullException("");
-            Action execMethod = dictionary[package.Method];
-            execMethod();
+            _ = _package ?? throw new ArgumentNullException("Ошибка при открытии пакета");
+            Action<Package> execMethod = dictionary[_package.Method];
+            execMethod(_package);
         }
-
-        public void Get()
+        
+        public static void Get(Package package)
         {
             Console.WriteLine("GET method вызван");
+            
         }
-        public void Post()
+        public static void Post(Package package)
         {
             Console.WriteLine("POST method вызван");
         }
