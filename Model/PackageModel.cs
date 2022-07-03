6789f1d6beaeb9ee3ff526ff0e3674772132221c
Alexander <Aleksandr.Dedyukhin@vsegda-da.com>
2022-07-03 15:15:48 +0400

    поправил

--- Model/PackageModel.cs
+++ Model/PackageModel.cs
@@ -2,13 +2,14 @@
 {
     public class PackageModel
     {
-        private static Dictionary<string, Action<Package>> dictionary = new Dictionary<string, Action<Package>>();       
+        private Dictionary<string, Action<Package>> dictionary;      
         public PackageModel()
         {
+            dictionary = new Dictionary<string, Action<Package>>();
             dictionary.Add("get", new Action<Package>(package => Get(package)));
             dictionary.Add("post", new Action<Package>(package => Post(package)));            
         }
-        public static void Execute(Package _package)
+        public void Execute(Package _package)
         {
             _ = _package ?? throw new ArgumentNullException("Ошибка при открытии пакета");
             Action<Package> execMethod = dictionary[_package.Method];
